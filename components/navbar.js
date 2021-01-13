34fba892ff0ed6c9e2e77e69518e0f06059f86c0
jareglaze <jarred.glaser@gmail.com>
2021-01-13 15:32:25 -0600

    add animated burger

---
+++ components/navbar.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react'
+import Hamburger from 'hamburger-react'
+
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [[width, height], setSize] = useState([0, 0]);
+  
+    useEffect(() => {
+      function updateSize() {
+        setSize([window.innerWidth, window.innerHeight]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+  
+    useEffect(() => {
+      if (width >= 640) {
+        setMenuOpen(false);
+      }
+    }, [width]);
+  
+    function NavbarItem(props) {
+      return (
+        <div className="nav-item"><a href={props.path}>{props.name}</a></div>
+      )
+    }
+  
+    const handleMenuOpen = () => {
+      if (width >= 640) {
+        return "";
+      }
+      
+      return menuOpen ? "open" : "close";
+    }
+  
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navbar-top">
+            <div className="logo">My site</div>
+            <div className="burger">
+                <Hamburger
+                    toggled={menuOpen} 
+                    toggle={setMenuOpen} 
+                    direction="right" 
+                    rounded />
+            </div>
+          </div>
+  
+          <div className={`nav-items ${handleMenuOpen()}`}>
+            <NavbarItem name="Home" path="#" />
+            <NavbarItem name="About" path="#" />
+            <NavbarItem name="Projects" path="#" />
+          </div>
+        </nav>
+      </>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,55 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState } from 'react'
+import Navbar from 'components/navbar'
+import Hamburger from 'hamburger-react'
 
 export default function Home() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [[width, height], setSize] = useState([0, 0]);
-
-  useEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-
-  useEffect(() => {
-    if (width >= 640) {
-      setMenuOpen(false);
-    }
-  }, [width]);
-
-  function NavbarItem(props) {
-    return (
-      <div className="nav-item"><a href={props.path}>{props.name}</a></div>
-    )
-  }
-
-  const handleMenuOpen = () => {
-    if (width >= 640) {
-      return "";
-    }
-    
-    return menuOpen ? "open" : "close";
-  }
-
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <div className="bg">
-      <nav className="navbar">
-        <div className="navbar-top">
-          <div className="logo">My site</div>
-          <img onClick={() => {setMenuOpen(!menuOpen)}} className="burger" src="/menu.svg" width="30px" height="30px"></img>
-        </div>
+    <>
+      <div className="bg">
+        <Navbar />
+        Hello!
 
-        <div className={`nav-items ${handleMenuOpen()}`}>
-          <NavbarItem name="Home" path="#" />
-          <NavbarItem name="About" path="#" />
-          <NavbarItem name="Projects" path="#" />
-        </div>
-      </nav>
+        <br></br>
 
-      <div className="content">
-        <span>Hello World</span>
-        <div>
-          width: {width} <br></br>
-          height {height}
-        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        fdhjahsfd
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        gfdhjkfhskj
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        fdjklsfjkl<br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>hfjdkshfjks
+        <br></br>
+        <br></br>
+        fdsafdas
+        jkldjklsdf
+        <br></br>
+        <br></br>
+        <br></br>hjgsdkh
       </div>
-    </div>
+    </>
   )
 }

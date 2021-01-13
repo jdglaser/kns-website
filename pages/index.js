23810e6442650f03c5c6216a8beebb66e4d09282
jareglaze <jarred.glaser@gmail.com>
2021-01-12 18:47:09 -0600

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, {useState, useEffect} from 'react'
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [[width, height], setSize] = useState([0, 0]);
+
+  useEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  useEffect(() => {
+    if (width >= 640) {
+      setMenuOpen(false);
+    }
+  }, [width]);
+
+  function NavbarItem(props) {
+    return (
+      <div className="nav-item"><a href={props.path}>{props.name}</a></div>
+    )
+  }
+
+  const handleMenuOpen = () => {
+    if (width >= 640) {
+      return "";
+    }
+    
+    return menuOpen ? "open" : "close";
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="bg">
+      <nav className="navbar">
+        <div className="navbar-top">
+          <div className="logo">My site</div>
+          <img onClick={() => {setMenuOpen(!menuOpen)}} className="burger" src="/menu.svg" width="30px" height="30px"></img>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={`nav-items ${handleMenuOpen()}`}>
+          <NavbarItem name="Home" path="#" />
+          <NavbarItem name="About" path="#" />
+          <NavbarItem name="Projects" path="#" />
+        </div>
+      </nav>
+
+      <div className="content">
+        <span>Hello World</span>
+        <div>
+          width: {width} <br></br>
+          height {height}
+        </div>
+      </div>
     </div>
   )
 }

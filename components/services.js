f7e4376cb336870527e5b808c87aac5987d2b77b
jareglaze <jarred.glaser@gmail.com>
2021-01-15 09:55:01 -0600

    fix service spacing

--- components/services.js
+++ components/services.js
@@ -4,7 +4,8 @@ import Button from 'components/button'
 export default function Services() {
     return (
         <div className="service">
-            <h2 id="service" style={{marginBottom:"0", marginLeft:"20px"}}>Exceptional Services.<br></br>Low Prices.</h2>
+            <h2 id="service" style={{marginBottom:"0", marginLeft:"20px"}}>
+                Our Exceptional Services</h2>
             
             <div className="service-cards">
                 <div className="card">
@@ -13,14 +14,13 @@ export default function Services() {
                             <img className="service-icon rake" src="/raking.png" width="150px" height="150px"></img>
                         </div>
                     </div>
-                    <div className="sub-item">
+                    <div className="sub-item lower">
                         <h2 className="service-title">Raking Leaves</h2>
                         <div className="price">
                             <span className="dollars">$25</span>
                             /yard
                         </div>
-                        <hr></hr>
-                        <p>Don't let the falling leaves get you down. Let us take care of all your raking needs!</p>
+                        <p className="service-description">Don't let the falling leaves get you down. Let us take care of all your raking needs!</p>
                         <div className="centered">
                             <Button text="Schedule Now!"/>
                         </div>
@@ -33,14 +33,13 @@ export default function Services() {
                             <img className="service-icon shovel" src="/shovel.png" width="150px" height="150px"></img>
                         </div>
                     </div>
-                    <div className="sub-item">
+                    <div className="sub-item lower">
                         <h2 className="service-title">Shoveling Snow</h2>
                         <div className="price">
                             <span className="dollars">$20</span>
                             /driveway
                         </div>
-                        <hr></hr>
-                        <p>Does the thought of shoveling give you chills? We've got your back!</p>
+                        <p className="service-description">Does the thought of shoveling give you chills? We've got your back!</p>
                         <div className="centered">
                             <Button text="Schedule Now!"/>
                         </div>
@@ -53,14 +52,13 @@ export default function Services() {
                             <img className="service-icon cat" src="/cat.png" width="100px" height="100px"></img>
                         </div>
                     </div>
-                    <div className="sub-item">
+                    <div className="sub-item lower">
                         <h2 className="service-title">Catsitting</h2>
                         <div className="price">
                             <span className="dollars">$20</span>
                             /hour
                         </div>
-                        <hr></hr>
-                        <p>In need of a caretaker for your favorite furry friend? Schedule our top of the line catsitting services now!</p>
+                        <p className="service-description">In need of a caretaker for your favorite furry friend? Catsitting services now available!</p>
                         <div className="centered">
                             <Button text="Schedule Now!"/>
                         </div>

b7a0422fb2f75d6609889059ad365f310d014803
jareglaze <jarred.glaser@gmail.com>
2021-01-17 11:52:40 -0600

    fix up spacing issues

--- components/contact.js
+++ components/contact.js
@@ -3,10 +3,7 @@ import React from 'react'
 export default function Contact() {
     return (
         <div id="contact" className="contact">
-            <h2 className="contact-header">Contact</h2>
-            <div className="contact-description">
-                If you have any questions or want to schedule a service, feel free to contact us using any of the methods below!
-            </div>
+            <h2 className="contact-header">Reach out now!</h2>
             <div className="contact-methods">
                 <div className="contact-method">
                     <div className="contact-icon-circle">
@@ -23,7 +20,7 @@ export default function Contact() {
                     </div>
                     <div className="email">
                         <div className="low-border-line">Email</div>
-                        <div>kns-services@gmail.com</div>
+                        <div><a href="mailto:kns-services@gmail.com">kns-services@gmail.com</a></div>
                     </div>
                 </div>
             </div>
--- components/reviews.js
+++ components/reviews.js
@@ -4,11 +4,11 @@ import Review from 'components/review'
 export default function Reviews() {
     return (
         <div id="reviews" className="reviews">
-            <h2 className="review-header">Don't just take our word for it</h2>
+            <h2 className="review-header">Don't just take our word for it...</h2>
             <div className="actual-reviews">
                 <Review review="Keegan helped me so much!" rating={5} name="- Daryl" />
-                <Review rating={4.5} review="Wow! My driveway is looking SO clean! I'm definitely making Keegan's neighborhood services my go-to." name="- John" />
                 <Review rating={5} review="My cat loves having Keegan come by to feed her. She is so happy, and so am I to get some much needed time away!" name="- Susan" />
+                <Review rating={4.5} review="Wow! My driveway is looking SO clean! I'm definitely using KNS again!" name="- John" />
             </div>
         </div>
     )

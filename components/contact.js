cd8bb186c6348edf6203db0aa75b5566b7bae8e3
jareglaze <jarred.glaser@gmail.com>
2021-01-28 17:39:05 -0600

    fix contact info

--- components/contact.js
+++ components/contact.js
@@ -6,23 +6,25 @@ export default function Contact() {
             <h2 className="contact-header">Reach out now!</h2>
             <div className="contact-methods">
                 <div className="contact-method">
-                    <div className="contact-icon-circle">
-                        <img className="phone-icon" src="/phone.svg"></img>
-                    </div>
+                    <a href="tel:920-422-8007">
+                        <div className="contact-icon-circle">
+                            <img className="phone-icon" src="/phone.svg"></img>
+                        </div>
+                    </a>
                     <div className="phone-number">
                         <div className="low-border-line">Call/Text</div>
-                        <div>920-422-8007</div>
+                        <a href="tel:920-422-8007"><div>920-422-8007</div></a>
                     </div>
                 </div>
                 <div className="contact-method">
-                    <a href="mailto:kns-services@gmail.com">
+                    <a href="mailto:keeganglaser@gmail.com">
                         <div className="contact-icon-circle">
                             <img className="email-icon" src="/email.svg"></img>
                         </div>
                     </a>
                     <div className="email">
                         <div className="low-border-line">Email</div>
-                        <div><a href="mailto:kns-services@gmail.com">keegan.glaser@gmail.com</a></div>
+                        <div><a href="mailto:keeganglaser@gmail.com">keeganglaser@gmail.com</a></div>
                     </div>
                 </div>
             </div>
